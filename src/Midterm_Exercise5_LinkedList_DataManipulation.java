1d01ccf8c08c15ec278c53915f8461a0b9a3c748
Abegail Torrendon <abby@picklesniffers.com>
2024-05-08 04:20:41 +0700

    Updated README.md and starting to rename files and classes for easy reading

--- src/Midterm_Exercise5_LinkedList_DataManipulation.java
+++ src/Midterm_Exercise5_LinkedList_DataManipulation.java
@@ -40,11 +40,67 @@ Condition: The program will only stop asking for the value to be searched and pe
  * 
 */
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Midterm_Exercise5_LinkedList_DataManipulation {
 	public Midterm_Exercise5_LinkedList_DataManipulation () {
-		// TODO Auto-generated constructor stub
 
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<String> list = new LinkedList<>();
 
+        int choice;
+        do {
+            System.out.print("Enter name to be added in our Linked List: ");
+            String name = scanner.nextLine();
+            list.add(name);
+
+            System.out.print("Add new record to our linked list [1-Yes|0-No] ? ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+        } while (choice == 1);
+
+        System.out.print("\nLet's show the records stored in our linked list ");
+        System.out.println(list);
+
+        int searchChoice;
+        do {
+            System.out.print("\nType in the name that you want to search: ");
+            String searchName = scanner.nextLine();
+            int index = list.indexOf(searchName);
+
+            if (index != -1) {
+                System.out.println("> Is [" + index + "]: " + list.get(index) + " == " + searchName + "? TRUE, stored at [" + index + "]");
+                System.out.print("\nWhat do you want to do [1-Update|2-Delete] ? ");
+                int actionChoice = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
+
+                if (actionChoice == 2) {
+                    list.remove(index);
+                } else if (actionChoice == 1) {
+                    System.out.print("New name to be stored: ");
+                    String newName = scanner.nextLine();
+                    list.set(index, newName);
+                }
+            } else {
+                System.out.println("> Is [" + searchName + "]: NOT FOUND");
+            }
+
+            System.out.print("\nUpdated records stored in our linked list ");
+            System.out.println(list);
+
+            // Check if all nodes are deleted
+            if (list.isEmpty()) {
+                System.out.println("\nAll records are deleted. Exiting program...");
+                break;
+            }
+
+            System.out.print("\nContinue searching and modifying records [1-Yes|0-No] ? ");
+            searchChoice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+        } while (searchChoice == 1);
+
+        //scanner.close();
 		
 	}
 }
